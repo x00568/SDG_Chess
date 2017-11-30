3d6b4a9f456d840a4fd6f72b7af22eef629a213a
x00568 <x100568@163.com>
2017-11-30 20:38:29 +0800

    将部分GUI绘制改为按钮控制

--- assets/Scripts/Square.cs
+++ assets/Scripts/Square.cs
@@ -1,53 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Square : MonoBehaviour {
-	public Color baseColor;
-	public Color rangeColor;
-	public Color attackColor;
+public class Square : MonoBehaviour
+{
+    public Color baseColor;
+    public Color rangeColor;
+    public Color attackColor;
 
-	private bool activo;
-	private Chess chess;	
+    private bool activo;
+    private Chess chess;
 
-	void Start(){
-		chess = GameObject.FindObjectOfType<Chess> ();
+    void Start()
+    {
+        chess = GameObject.FindObjectOfType<Chess>();
 
-		activo = false;
-	}
+        activo = false;
+    }
 
-	public void resetColor(){
-		gameObject.GetComponent<Renderer>().material.color = baseColor;
-		activo = false;
-	}
-	
-	public void attack(){
-		gameObject.GetComponent<Renderer>().material.color = attackColor;
-		activo = true;
-	}
-	
-	public void range(){
-		gameObject.GetComponent<Renderer>().material.color = rangeColor;
-		activo = true;
-	}
-	
-	void OnMouseEnter(){
-		if (activo) {
-			chess.lightAttackers(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z), true);
-		}
-	}
-	
-	void OnMouseExit(){
-		if (activo) {
-			chess.lightAttackers(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z), false);
-		}
-	}
-	
-	void OnMouseDown(){
-		if (activo) {
-			chess.lightAttackers(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z), false);
-			chess.moveUser(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
-		}else{
-			chess.selectUser(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
-		}
-	}
+    public void resetColor()
+    {
+        gameObject.GetComponent<Renderer>().material.color = baseColor;
+        activo = false;
+    }
+
+    public void attack()
+    {
+        gameObject.GetComponent<Renderer>().material.color = attackColor;
+        activo = true;
+    }
+
+    public void range()
+    {
+        gameObject.GetComponent<Renderer>().material.color = rangeColor;
+        activo = true;
+    }
+
+    void OnMouseEnter()
+    {
+        if (activo)
+        {
+            chess.lightAttackers(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z), true);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (activo)
+        {
+            chess.lightAttackers(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z), false);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (activo)
+        {
+            chess.lightAttackers(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z), false);
+            chess.moveUser(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
+        }
+        else
+        {
+            chess.selectUser(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
+        }
+    }
 }
